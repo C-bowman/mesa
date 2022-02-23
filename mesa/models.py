dc359221b2035c97d046d733ce4cd548895d75b4
C-bowman <chris.bowman.physics@gmail.com>
2022-02-23 22:45:27 +0000

    Code formatting improvements for models module

--- mesa/models.py
+++ mesa/models.py
@@ -1,5 +1,6 @@
 from numpy import exp, piecewise, linspace, concatenate
 
+
 def left_section_exp(x, h0, h1, lam, x0):
     return (1 - exp(lam*(x-x0)))*(h0 - h1) + h1
 
@@ -17,35 +18,37 @@ def linear_section(x, m, c):
 
 
 def exponential_transport_profile(x, params):
-    L_asymp = params[0] # left asymptote height
-    R_asymp = params[1] # right asymptote height
-    L_shape = params[2] # left-side decay rate
-    R_shape = params[3] # right-side decay rate
-    L_bound = params[4] - params[5] # boundary point between left and middle sections
-    R_bound = params[4] + params[5] # boundary point between middle and right sections
-    M_height= params[6] # height of the middle section
+    L_asymp = params[0]  # left asymptote height
+    R_asymp = params[1]  # right asymptote height
+    L_shape = params[2]  # left-side decay rate
+    R_shape = params[3]  # right-side decay rate
+    L_bound = params[4] - params[5]  # boundary point between left and middle sections
+    R_bound = params[4] + params[5]  # boundary point between middle and right sections
+    M_height= params[6]  # height of the middle section
 
     in_left = x < L_bound
     in_right = x > R_bound
     in_middle = ~(in_left | in_right)
     conditions = [in_left, in_middle, in_right]
-    functions = [ lambda x : left_section_exp(x, L_asymp, M_height, L_shape, L_bound),
-                  lambda x : middle_section(x, M_height),
-                  lambda x : right_section_exp(x, R_asymp, M_height, R_shape, R_bound) ]
+    functions = [
+        lambda x : left_section_exp(x, L_asymp, M_height, L_shape, L_bound),
+        lambda x : middle_section(x, M_height),
+        lambda x : right_section_exp(x, R_asymp, M_height, R_shape, R_bound)
+    ]
 
     return piecewise(x, conditions, functions)
 
 
 def linear_transport_profile(x, params):
     M_height = params[5]  # height of the middle section
-    L_asymp = params[0] + M_height # left boundary height
-    R_asymp = params[1] + M_height # right asymptote height
-    L_mid   = params[2]*L_asymp + (1.-params[2])*M_height # left-middle height
-    R_mid   = params[3]*R_asymp + (1.-params[3])*M_height # left-middle height
-    L_bound = params[4] - 0.5*params[6] # boundary point between left and middle sections
-    R_bound = params[4] + 0.5*params[6] # boundary point between middle and right sections
-    L_gap = params[7] # left-mid-point gap from LM boundary
-    R_gap = params[8] # right-mid-point gap from MR boundary
+    L_asymp = params[0] + M_height  # left boundary height
+    R_asymp = params[1] + M_height  # right asymptote height
+    L_mid = params[2]*L_asymp + (1.-params[2])*M_height  # left-middle height
+    R_mid = params[3]*R_asymp + (1.-params[3])*M_height  # left-middle height
+    L_bound = params[4] - 0.5*params[6]  # boundary point between left and middle sections
+    R_bound = params[4] + 0.5*params[6]  # boundary point between middle and right sections
+    L_gap = params[7]  # left-mid-point gap from LM boundary
+    R_gap = params[8]  # right-mid-point gap from MR boundary
 
     # construct boolean arrays of the conditions
     in_left = x < L_bound - L_gap
@@ -68,21 +71,23 @@ def linear_transport_profile(x, params):
     MR_c = R_mid - MR_m*(R_bound + R_gap)
 
     # build functions for each section
-    functions = [ lambda x : linear_section(x, L_m, L_c),
-                  lambda x : linear_section(x, ML_m, ML_c),
-                  lambda x : middle_section(x, M_height),
-                  lambda x : linear_section(x, MR_m, MR_c),
-                  lambda x : linear_section(x, R_m, R_c) ]
+    functions = [
+        lambda x: linear_section(x, L_m, L_c),
+        lambda x: linear_section(x, ML_m, ML_c),
+        lambda x: middle_section(x, M_height),
+        lambda x: linear_section(x, MR_m, MR_c),
+        lambda x: linear_section(x, R_m, R_c)
+    ]
 
     return piecewise(x, conditions, functions)
 
 
-def profile_radius_axis(logarithmic = True, N_points = 32):
+def profile_radius_axis(logarithmic=True, N_points=32):
     if logarithmic:
-        grd = exp(-linspace(0.0,3.0,N_points//2))
+        grd = exp(-linspace(0.0, 3.0, N_points//2))
         grd = grd-min(grd)+0.01
         grd = (-0.1)*grd/max(grd)
-        radius = concatenate([grd,-grd[::-1]])
+        radius = concatenate([grd, -grd[::-1]])
     else:
         radius = linspace(-0.1, 0.1, N_points)
 
