dc944d85bec87b5e727b71f0ee8827fee786f3a7
Jarrod Leddy <jleddy@Tech-Xs-MacBook-Pro.local>
2022-11-09 16:05:03 -0500

    add data interface method to simulation object for use with updating interface on diagnostic

--- mesa/simulations/simulation.py
+++ mesa/simulations/simulation.py
@@ -1,8 +1,8 @@
 import subprocess
-import os
 from os.path import isfile
+from abc import ABC
 
-class Simulation():
+class Simulation(ABC):
     def __init__(self,
         exe=None,
         n_proc=1
@@ -11,6 +11,11 @@ class Simulation():
         self.n_proc = n_proc
         self.timeout_hours = timeout_hours
         self.concurrent_runs = concurrent_runs
+        self.output_filename = None # should be overwritten by derived classes
+
+    @abstractmethod
+    def get_data(self,path=path):
+        pass
 
     def create_case_directory(self,iteration,ref_directory,inputfiles):
         self.casedir = ref_directory + f"iteration_{iteration}/"
