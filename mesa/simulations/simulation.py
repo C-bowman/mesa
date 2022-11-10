78332d494630fcaee237c972a27ec408e0aaba17
Jarrod Leddy <jleddy@Tech-Xs-MacBook-Pro.local>
2022-11-10 10:22:31 -0500

    fix imports. Add base class for SimulationRun that SolpsRun derives from - this includes the same class data but allows for different checks regarding simulation status

--- mesa/simulations/simulation.py
+++ mesa/simulations/simulation.py
@@ -1,8 +1,16 @@
 import subprocess
 from os.path import isfile
 from abc import ABC
+import logging
 
 class Simulation(ABC):
+    exe: str
+    n_proc: int
+    timeout_hours: float
+    concurrent_runs: int
+    output_filename: str
+    reference_dir: str
+
     def __init__(self,
         exe=None,
         n_proc=1
@@ -19,6 +27,7 @@ class Simulation(ABC):
 
     def create_case_directory(self,iteration,ref_directory,inputfiles):
         self.casedir = ref_directory + f"iteration_{iteration}/"
+        self.reference_dir = ref_directory
 
         # create the case directory and copy all the reference files
         subprocess.run(["mkdir", self.casedir])
@@ -26,3 +35,29 @@ class Simulation(ABC):
         for input in inputfiles:
             if isfile(ref_directory + input):
                 subprocess.run(["cp", ref_directory + input, self.casedir + input])
+
+@dataclass(frozen=True)
+class SimulationRun:
+    run_id: str
+    directory: str
+    parameters: dict
+    iteration: int
+    launch_time: float
+    timeout_hours: float
+    job_queue = None
+
+    def status(self):
+        whoami = subprocess.run("whoami", capture_output=True, encoding="utf-8")
+        username = whoami.stdout.rstrip()
+
+        squeue = subprocess.run(["squeue", "-u", username], capture_output=True, encoding="utf-8")
+        self.job_queue = squeue.stdout
+
+    def cancel(self):
+        subprocess.run(["scancel", self.run_id])
+
+    def __key(self):
+        return self.run_id, self.directory, self.iteration, self.launch_time
+
+    def __hash__(self):
+        return hash(self.__key())
