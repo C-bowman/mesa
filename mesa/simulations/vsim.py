6d9ce3175dfde7c6d04ef699b204524025cddbf2
Jarrod Leddy <jleddy@Tech-Xs-MacBook-Pro.local>
2022-11-21 20:09:37 -0500

    remove unused code, change cleanup call, move imports

--- mesa/simulations/vsim.py
+++ mesa/simulations/vsim.py
@@ -4,13 +4,15 @@ from time import time
 import logging
 import subprocess
 from dataclasses import dataclass
-from numpy import sum
-
-from sims.interface import SolpsInterface
-from sims.likelihoods import gaussian_likelihood, cauchy_likelihood, laplace_likelihood, logistic_likelihood
 
 from mesa.simulations.simulation import Simulation
 from mesa.simulations.simulation import SimulationRun
+
+class VSimInterface():
+    path : str
+
+    def __init__(self,path):
+        self.path = path
 
 @dataclass(frozen=True)
 class VSimRun(SimulationRun):
@@ -28,17 +30,19 @@ class VSimRun(SimulationRun):
 
     def cleanup(self):
         output_files = [f for f in os.listdir(self.directory) if isfile(self.directory + f)]
-        allowed_files = (
-            "balance.nc", "input.dat", "b2.neutrals.parameters",
-            "b2.boundary.parameters", "b2.numerics.parameters",
-            "b2.transport.parameters", "b2.transport.inputfile",
-            "b2mn.dat"
-        )
-        deletions = [f for f in output_files if f not in allowed_files]
-        [os.remove(self.directory + f) for f in deletions]
+        return
+        # allowed_files = (
+        #     "balance.nc", "input.dat", "b2.neutrals.parameters",
+        #     "b2.boundary.parameters", "b2.numerics.parameters",
+        #     "b2.transport.parameters", "b2.transport.inputfile",
+        #     "b2mn.dat"
+        # )
+        # deletions = [f for f in output_files if f not in allowed_files]
+        # [os.remove(self.directory + f) for f in deletions]
 
 class VSim(Simulation):
-    input_file : str
+    base_input_file : str
+    slurm : bool
 
     def __init__(self,
         exe=None,
@@ -96,8 +100,6 @@ class VSim(Simulation):
         # Go to the SOLPS run directory to prepare execution
         os.chdir(self.casedir)
 
-        findstr = 'Submitted batch job'
-
         # source
         try:
             src = subprocess.Popen("source "+self.exe, stdout=subprocess.PIPE, shell=True)
@@ -117,11 +119,16 @@ class VSim(Simulation):
         start_run_output = start_run.communicate()[0]
         os.chdir(self.reference_dir)
         # Find the batch job number
-        tmp = str(start_run_output).find(findstr)
-        job_id = str(start_run_output)[tmp+len(findstr)+1:-3]
+        if (self.slurm):
+            findstr='r'
+            tmp = str(start_run_output).find(findstr)
+            job_id = str(start_run_output)[tmp+len(findstr)+1:-3]
+            logging.info(f'[vsim_interface] Submitted job {job_id}')
+        else:
+            job_id = 0
+            logging.info(f'[vsim_interface] Running job {iteration}')
 
-        logging.info(f'[solps_interface] Submitted job {job_id}')
-        return SolpsRun(
+        return VSimRun(
             run_id=job_id,
             directory=self.casedir,
             iteration=iteration,
@@ -138,188 +145,7 @@ class VSim(Simulation):
             raise ValueError(
                 f"""
                 [ MESA ERROR ]
-                >> Path not provided for simulation data in SOLPS get_data()
+                >> Path not provided for simulation data in VSim get_data()
                 """
             )
-        return SolpsInterface(path+"balance.nc")
-
-    conductivity_profile = (
-        "chi_boundary_left",
-        "chi_boundary_right",
-        "chi_frac_left",
-        "chi_frac_right",
-        "chi_barrier_centre",
-        "chi_barrier_height",
-        "chi_barrier_width",
-        "chi_gap_left",
-        "chi_gap_right",
-    )
-
-    diffusivity_profile = (
-        "D_boundary_left",
-        "D_boundary_right",
-        "D_frac_left",
-        "D_frac_right",
-        "D_barrier_centre",
-        "D_barrier_height",
-        "D_barrier_width",
-        "D_gap_left",
-        "D_gap_right",
-    )
-
-    required_parameters = {
-        "chi_boundary_left",
-        "chi_boundary_right",
-        "chi_frac_left",
-        "chi_frac_right",
-        "chi_barrier_centre",
-        "chi_barrier_height",
-        "chi_barrier_width",
-        "chi_gap_left",
-        "chi_gap_right",
-        "D_boundary_left",
-        "D_boundary_right",
-        "D_frac_left",
-        "D_frac_right",
-        "D_barrier_centre",
-        "D_barrier_height",
-        "D_barrier_width",
-        "D_gap_left",
-        "D_gap_right",
-    }
-
-    divertor_transport = ("D_div", "chi_div")
-
-    dataframe_columns = (
-        "iteration",
-        "gaussian_logprob",
-        "cauchy_logprob",
-        "laplace_logprob",
-        "logistic_logprob",
-        "error_model",
-        "cross_validation",
-        "acquisition_function",
-        "prediction_mean",
-        "prediction_error",
-        "convergence_metric",
-    )
-
-    from numpy import array, exp, piecewise, concatenate, sort
-
-    def left_section_exp(x, h0, h1, lam, x0):
-        return (1 - exp(lam*(x-x0)))*(h0 - h1) + h1
-
-
-    def middle_section(x, h):
-        return h
-
-
-    def right_section_exp(x, h0, h1, lam, x0):
-        return (1 - exp(-lam*(x-x0)))*(h0 - h1) + h1
-
-
-    def linear_section(x, m, c):
-        return m*x + c
-
-    def exponential_transport_profile(x, params):
-        L_asymp = params[0]  # left asymptote height
-        R_asymp = params[1]  # right asymptote height
-        L_shape = params[2]  # left-side decay rate
-        R_shape = params[3]  # right-side decay rate
-        L_bound = params[4] - params[5]  # boundary point between left and middle sections
-        R_bound = params[4] + params[5]  # boundary point between middle and right sections
-        M_height= params[6]  # height of the middle section
-
-        in_left = x < L_bound
-        in_right = x > R_bound
-        in_middle = ~(in_left | in_right)
-        conditions = [in_left, in_middle, in_right]
-        functions = [
-            lambda x: left_section_exp(x, L_asymp, M_height, L_shape, L_bound),
-            lambda x: middle_section(x, M_height),
-            lambda x: right_section_exp(x, R_asymp, M_height, R_shape, R_bound)
-        ]
-
-        return piecewise(x, conditions, functions)
-
-
-    def linear_profile_knots(params, boundaries):
-        knot_locations = array([
-            boundaries[0],  # left edge
-            boundaries[1],  # right edge
-            params[4] - 0.5*params[6],
-            params[4] + 0.5*params[6],
-            params[4] - 0.5*params[6] - params[7],
-            params[4] + 0.5*params[6] + params[8],
-        ])
-
-        knot_values = array([
-            params[0] + params[5],
-            params[1] + params[5],
-            params[5],
-            params[5],
-            params[5] + params[2]*params[0],
-            params[5] + params[3]*params[1]
-        ])
-        sorter = knot_locations.argsort()
-        return knot_locations[sorter], knot_values[sorter]
-
-
-    def linear_transport_profile(x, params, boundaries=(-0.1, 0.1)):
-        M_height = params[5]  # height of the middle section
-        L_asymp = params[0] + M_height  # left boundary height
-        R_asymp = params[1] + M_height  # right asymptote height
-        L_mid = params[2]*params[0] + M_height  # left-middle height
-        R_mid = params[3]*params[1] + M_height  # left-middle height
-        L_bound = params[4] - 0.5*params[6]  # boundary point between left and middle sections
-        R_bound = params[4] + 0.5*params[6]  # boundary point between middle and right sections
-        L_gap = params[7]  # left-mid-point gap from LM boundary
-        R_gap = params[8]  # right-mid-point gap from MR boundary
-
-        # construct boolean arrays of the conditions
-        in_left = x < L_bound - L_gap
-        in_right = x > R_bound + R_gap
-        in_left_mid = (x < L_bound) & (x >= L_bound - L_gap)
-        in_right_mid = (x > R_bound) & (x <= R_bound + R_gap)
-        in_middle = ~(in_left | in_right | in_left_mid | in_right_mid)
-        conditions = [in_left, in_left_mid, in_middle, in_right_mid, in_right]
-
-        # get the line gradients for each section
-        Lx, Rx = boundaries
-        L_m = (L_mid - L_asymp) / (L_bound - L_gap - Lx)
-        R_m = (R_asymp - R_mid) / (Rx - R_bound - R_gap)
-        ML_m = (M_height - L_mid) / L_gap
-        MR_m = (R_mid - M_height) / R_gap
-
-        # get the line y-intercepts for each section
-        L_c = L_asymp - L_m*Lx
-        R_c = R_asymp - R_m*Rx
-        ML_c = L_mid - ML_m*(L_bound - L_gap)
-        MR_c = R_mid - MR_m*(R_bound + R_gap)
-
-        # build functions for each section
-        functions = [
-            lambda x: linear_section(x, L_m, L_c),
-            lambda x: linear_section(x, ML_m, ML_c),
-            lambda x: middle_section(x, M_height),
-            lambda x: linear_section(x, MR_m, MR_c),
-            lambda x: linear_section(x, R_m, R_c)
-        ]
-
-        return piecewise(x, conditions, functions)
-
-
-    def profile_radius_axis(params, boundaries):
-        r, _ = linear_profile_knots(params, boundaries)
-        # find the spacing that we'll use to place points around each knot
-        dr = [r[1] - r[0]]
-        for i in range(1, 5):
-            dr.append(min(r[i] - r[i-1], r[i+1] - r[i]))
-        dr.append(r[5] - r[4])
-        # generate the points around each knot
-        spacing = array([-6., -2., -1., 1., 2., 6.]) * 0.03
-        left_edge = spacing[3:]*dr[0] + r[0]
-        right_edge = spacing[:3]*dr[-1] + r[-1]
-        middles = [spacing*dr[i] + r[i] for i in range(1, 5)]
-        # combine all the points and return them sorted
-        return sort(concatenate([left_edge, *middles, right_edge, boundaries]))
+        return VSimInterface(path)
