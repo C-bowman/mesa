d163fedf65c1dced605850f01baef5f03969c5a5
C-bowman <chris.bowman.physics@gmail.com>
2023-01-23 09:44:24 +0000

    cleanup of unused imports

--- mesa/__init__.py
+++ mesa/__init__.py
@@ -1,16 +1,7 @@
-
 import os
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
 os.environ["OMP_NUM_THREADS"] = "1"
-
-from numpy import array
-from numpy.random import random
-from pandas import DataFrame, read_hdf
-
-import subprocess
-from time import sleep
-from os.path import isfile
 import logging
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
