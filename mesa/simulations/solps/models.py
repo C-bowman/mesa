47b12243def37533809159ad398fd8bcde235498
C-bowman <chris.bowman.physics@gmail.com>
2024-08-14 09:26:05 +0100

    Removed obsolete exponential transport profile model

--- mesa/simulations/solps/models.py
+++ mesa/simulations/solps/models.py
@@ -1,42 +1,12 @@
-from numpy import array, exp, piecewise, concatenate, sort
-
-
-def left_section_exp(x, h0, h1, lam, x0):
-    return (1 - exp(lam * (x - x0))) * (h0 - h1) + h1
+from numpy import array, piecewise, concatenate, sort
 
 
 def middle_section(x, h):
     return h
 
 
-def right_section_exp(x, h0, h1, lam, x0):
-    return (1 - exp(-lam * (x - x0))) * (h0 - h1) + h1
-
-
 def linear_section(x, m, c):
     return m * x + c
-
-
-def exponential_transport_profile(x, params):
-    L_asymp = params[0]  # left asymptote height
-    R_asymp = params[1]  # right asymptote height
-    L_shape = params[2]  # left-side decay rate
-    R_shape = params[3]  # right-side decay rate
-    L_bound = params[4] - params[5]  # boundary point between left and middle sections
-    R_bound = params[4] + params[5]  # boundary point between middle and right sections
-    M_height = params[6]  # height of the middle section
-
-    in_left = x < L_bound
-    in_right = x > R_bound
-    in_middle = ~(in_left | in_right)
-    conditions = [in_left, in_middle, in_right]
-    functions = [
-        lambda x: left_section_exp(x, L_asymp, M_height, L_shape, L_bound),
-        lambda x: middle_section(x, M_height),
-        lambda x: right_section_exp(x, R_asymp, M_height, R_shape, R_bound),
-    ]
-
-    return piecewise(x, conditions, functions)
 
 
 def linear_profile_knots(params, boundaries):
