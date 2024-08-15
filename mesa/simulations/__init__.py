4594c6188abfb5d7ec034410c92ccb5cee379426
C-bowman <chris.bowman.physics@gmail.com>
2024-08-15 09:22:28 +0100

    Switched SOLPS driver to track run number instead of iteration

--- mesa/simulations/__init__.py
+++ mesa/simulations/__init__.py
@@ -30,7 +30,7 @@ class SimulationRun(ABC):
         subprocess.run(["scancel", self.run_id])
 
     def __key(self):
-        return self.run_id, self.directory, self.iteration, self.launch_time
+        return self.run_id, self.directory, self.run_number, self.launch_time
 
     def __hash__(self):
         return hash(self.__key())
