ea160f8133d8aa5e35da399c0800d63b27b7c2d3
C-bowman <chris.bowman.physics@gmail.com>
2024-08-28 12:28:12 +0100

    Removed obsolete import

--- mesa/strategies/__init__.py
+++ mesa/strategies/__init__.py
@@ -1,3 +1,3 @@
-from mesa.strategies.base import Strategy, Optimizer
+from mesa.strategies.base import Strategy
 from mesa.strategies.gpo import GPOptimizer
 from mesa.strategies.genetic import GeneticOptimizer
