b0370ed7bac4d6d4c0c29b1978c583736d8b7b74
C-bowman <chris.bowman.physics@gmail.com>
2024-08-29 06:50:13 +0100

    Redesign of objective function classes

--- mesa/diagnostics.py
+++ mesa/diagnostics.py
@@ -1,52 +1,40 @@
 from abc import ABC, abstractmethod
-from numpy import sum
 import numpy as np
-from sims.likelihoods import gaussian_likelihood
 
 
-class Diagnostic(ABC):
+class ObjectiveFunction(ABC):
     """
     Abstract base class for all diagnostic objects
     """
 
     @abstractmethod
-    def update_interface(self, data):
-        pass
-
-    @abstractmethod
-    def log_likelihood(self, likelihood=None):
+    def evaluate(self, simulation_interface) -> dict[str, float]:
         pass
 
 
-class WeightedObjectiveFunction:
-    def __init__(self, diagnostics: list, weights: list = None):
-        self.diagnostics = diagnostics
+class WeightedObjective(ObjectiveFunction):
+    def __init__(self, objectives: list[ObjectiveFunction], weights: list[float] = None):
+        self.objectives = objectives
         self.weights = weights
 
         if self.weights is None:
-            self.weights = [1.0 for _ in self.diagnostics]
+            self.weights = [1.0 for _ in self.objectives]
 
-        if len(self.weights) != len(self.diagnostics):
+        if len(self.weights) != len(self.objectives):
             raise ValueError(
                 f"""
                 [ MESA ERROR ]
                 >> In WeightedObjectiveFunction, the number of diagnostics specified 
-                >> is {len(self.diagnostics)}, but the number of weights specified in
+                >> is {len(self.objectives)}, but the number of weights specified in
                 >> is {len(self.weights)}. These must be the same.
                 """
             )
 
-    def evaluate(self, simulation_interface=None):
-        # update the diagnostics with the latest data
-        for dia in self.diagnostics:
-            dia.update_interface(simulation_interface)
-
+    def evaluate(self, simulation_interface) -> dict[str, float]:
         return {
-            "logprob": sum(
-                [
-                    wgt * dia.log_likelihood(likelihood=gaussian_likelihood)
-                    for dia, wgt in zip(self.diagnostics, self.weights)
-                ]
+            "objective_value": sum(
+                weight * objective.evaluate(simulation_interface)["objective_value"]
+                for objective, weight in zip(self.objectives, self.weights)
             )
         }
 
