eaf322c5c0819b7f41361fd1e6577808340fe38e
C-bowman <chris.bowman.physics@gmail.com>
2024-09-04 12:26:00 +0100

    removed obsolete `Spectrum` diagnostic

--- mesa/diagnostics.py
+++ mesa/diagnostics.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-import numpy as np
 
 
 class ObjectiveFunction(ABC):
@@ -37,50 +36,3 @@ class WeightedObjective(ObjectiveFunction):
                 for objective, weight in zip(self.objectives, self.weights)
             )
         }
-
-
-class Spectrum(Diagnostic):
-    def __init__(self, frequency: float, goal: str):
-        self.frequency = frequency
-        self.goal = goal
-
-        if self.goal not in ["minimize", "maximize"]:
-            raise ValueError(
-                f"""
-                [ MESA ERROR ]
-                >> Spectrum goal should either be "minimize" or "maximize"
-                """
-            )
-
-        if type(self.frequency) is not float:
-            raise ValueError(
-                f"""
-                [ MESA ERROR ]
-                >> Spectrum frequency should be a float
-                """
-            )
-
-    def update_interface(self, data):
-        dataslice = [0.5, 0.5, 0.5]
-        self.E = data.getE(dataslice)
-        self.t = data.getTime()
-        self.ff = np.fft.fft(E, axis=t)
-        self.freqaxis = np.fft.fftfreq(t, d=self.t[1] - self.t[0])
-
-    def log_likelihood(self, likelihood=None) -> float:
-        ilow = 0
-        ihigh = len(self.freqaxis)
-        for i, f in enumerate(self.freqaxis):
-            if f < self.frequency and ilow < i:
-                ilow = i
-            elif f > self.frequency and ihigh > i:
-                ihigh = i
-        wgt = (self.freqaxis[ihigh] - self.frequency) / (
-            self.freqaxis[ihigh] - self.freqaxis[ilow]
-        )
-        content = wgt * self.ff[ilow] + (1.0 - wgt) * self.ff[ihigh]
-
-        if self.goal == "maximize":
-            return 1.0 - content
-        else:
-            return content
